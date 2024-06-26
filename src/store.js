55ff4fb5d66f54fdf3b576324d05881680893ee6
srujan0404 <srujan.23bcs10107@ms.sst.scaler.com>
2024-06-26 11:25:35 +0530

    changes in store.js

--- src/store.js
+++ src/store.js
@@ -1,6 +1,25 @@
 import { act } from "react";
 import { createStore } from "redux";
 import { omit } from "lodash";
+
+const ADD_TO_CART = "ADD_TO_CART_ONE";
+const REMOVE_FROM_CART = "REMOVE_FROM_CART";
+
+export function addToCart(product){
+  return {
+    type: ADD_TO_CART,
+    payload: product
+  }
+}
+
+export function removeTOCart(product){
+  return{
+    type: REMOVE_FROM_CART,
+    payload: product
+  }
+}
+
+
 function cartReducer(state= { items: {}}, action) {
   switch (action.type) {
     case "ADD_TO_CART": {
